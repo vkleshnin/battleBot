903846a355bc2b2716dc471e05f7cfc8596b1cc1
Владимир Клешнин <kleshnin99@gmail.com>
2023-10-07 04:19:45 +0300

    Rework start program.

--- BattleBot/Program.cs
+++ BattleBot/Program.cs
@@ -6,17 +6,19 @@ namespace BattleBot;
 
 public static class Program
 {
-	private static ITelegramBotClient _botClient;
-	private static ErrorHandler _errorHandler;
-	private static UpdateHandler _updateHandler;
-	private static ReceiverOptions _receiverOptions;
-
-	private const string TOKEN = "6576435744:AAHpfjXc5iebQERrtiBneuuQglfn7MnUl-E";
+	private static TelegramBotClient? _botClient;
+	
+	private const string ACCESS_TOKEN = "6576435744:AAHpfjXc5iebQERrtiBneuuQglfn7MnUl-E";
 
 	private static async Task Main()
 	{
-		_botClient = new TelegramBotClient(TOKEN);
-		_receiverOptions = new ReceiverOptions()
+		_botClient = new TelegramBotClient(ACCESS_TOKEN);
+		using var cts = new CancellationTokenSource();
+
+		var updateHandler = new UpdateHandler(); //создание объекта обработчика обновлений
+		var errorHandler = new ErrorHandler(); //создание объекта обработчика ошибок
+		
+		var receiverOptions = new ReceiverOptions()
 		{
 			AllowedUpdates = new []
 			{
@@ -25,17 +27,12 @@ public static class Program
 			},
 			ThrowPendingUpdates = true //параметр, отвечающий за обработку сообщений, если бот был offline
 		};
-
-		_errorHandler = new ErrorHandler(); //обработчик ошибок
-		_updateHandler = new UpdateHandler(); //обработчик изменений
-
-		using var cts = new CancellationTokenSource();
-        
-		_botClient.StartReceiving(_updateHandler.Update, _errorHandler.Error, _receiverOptions, cts.Token);
-
+		
+		_botClient.StartReceiving(updateHandler.Update, errorHandler.Error, receiverOptions, cts.Token);
+		
 		var me = await _botClient.GetMeAsync(cancellationToken: cts.Token);
 		Console.WriteLine($"{me.FirstName} is started.");
-        
+		
 		await Task.Delay(-1, cts.Token);
 	}
 }
